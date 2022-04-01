2f10c56ef1093ae896aa7756cf217810aac86e97
chiara <chiara.babiak@gmail.com>
2022-04-01 12:53:56 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# %matplotlib inline
+###############################################
+#                    IMPORTS                  #
+###############################################
+
+import numpy as np
+# import opencv module
+import cv2 as cv
+import matplotlib
+from matplotlib import colors
+from matplotlib import pyplot as plt
+from __future__ import division
+import pandas as pd
+
+
+
