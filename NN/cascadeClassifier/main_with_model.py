2657c750f6ec6de191e3def64367c4288dc730c1
chiara <chiara.babiak@gmail.com>
2022-04-01 21:54:47 +0200

    Cascade Model tested , not good yet , not enough data collected

--- NN/cascadeClassifier/main_with_model.py
+++ NN/cascadeClassifier/main_with_model.py
@@ -1,28 +1,53 @@
 # Once you have trained the model using the cascade trainger and providing all the positive and negative images then
 # Use this for using the trained model
 
+# testing model on static image
+# since not enough data => model is rlly bad
+
 import cv2
+import cap as cap
 
-cascade_src = 'cascade.xml'  # This should be the model.xml file obtained from the cascade trainger and use the correct name here
+# Capturing video through webcam
+webcam = cv2.VideoCapture(0)
 
-cap = cv2.VideoCapture(0) # if try on live use 0 instead
-model_cascade = cv2.CascadeClassifier(cascade_src) #Using the cascade classifier
+# load the required model
+model_cascade = cv2.CascadeClassifier('data/images/classifier/cascade.xml')
 
-while True:
-    ret, img = cap.read()
-    if (type(img) == type(None)):
+# Start a while loop
+while (1):
+    # Reading the video from the
+    # webcam in image frames
+    _, imageFrame = webcam.read()
+
+    # Converting image to grayscale
+    gray_img = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2GRAY)
+
+    strawberry = model_cascade.detectMultiScale(gray_img, 1.1, 9)
+
+    for (x, y, w, h) in strawberry:
+        cv2.rectangle(imageFrame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # Program Termination
+    cv2.imshow("Strawberry detection by Cascade Model", imageFrame)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
         break
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    model = model_cascade.detectMultiScale(gray, 1.1, 1)
 
-    for (x, y, w, h) in cars:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2) # drawing rectangle on detected area
+# Reading the image
+# img = cv2.imread('data/images/p/How-Many-Strawberries-Do-Strawberry-Plants-Produce-3.jpg')
 
-    cv2.imshow('video', img)
+# Converting image to grayscale
+# gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    if cv2.waitKey(33) == 27:
-        break
+# Loading the required haar-cascade xml classifier file
+# haar_cascade = cv2.CascadeClassifier('data/images/classifier/cascade.xml')
 
-cv2.destroyAllWindows()
+# Applying the face detection method on the grayscale image
+# faces_rect = haar_cascade.detectMultiScale(gray_img, 1.1, 9)
+
+# Iterating through rectangles of detected faces
+# for (x, y, w, h) in faces_rect:
+#    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
